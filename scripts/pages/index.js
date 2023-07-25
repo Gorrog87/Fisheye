19d8671fd193f523e11a2bdbfaf9e467d7008d52
Gorrog <maximegourgues@protonmail.com>
2023-07-25 12:09:22 +0200

    started to gete some data from the api to display it just for a first test

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,6 +1,15 @@
     async function getPhotographers() {
         // Ceci est un exemple de données pour avoir un affichage de photographes de test dès le démarrage du projet, 
         // mais il sera à remplacer avec une requête sur le fichier JSON en utilisant "fetch".
+        fetch("./data/photographers.json")
+        .then((response) => response.json())
+        .then((data) => {
+            const photographers = data.photographers;
+            console.log(photographers);
+            const photographerName = photographers.map(photographer => photographer.name);
+            const photographerCity = photographers.map(photographer => photographer.city);
+            
+        });
         let photographers = [
             {
                 "name": "Ma data test",
