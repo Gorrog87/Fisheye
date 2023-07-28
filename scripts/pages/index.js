0b18cc12e8d81a7fda2c7d448e218c5eb4079f47
Gorrog <maximegourgues@protonmail.com>
2023-07-28 17:24:13 +0200

    fetched correctly the json document an displayed it properly on browser

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,39 +1,45 @@
     async function getPhotographers() {
         // Ceci est un exemple de données pour avoir un affichage de photographes de test dès le démarrage du projet, 
         // mais il sera à remplacer avec une requête sur le fichier JSON en utilisant "fetch".
-        fetch("./data/photographers.json")
-        .then((response) => response.json())
-        .then((data) => {
-            const photographers = data.photographers;
-            console.log(photographers);
-            const photographerName = photographers.map(photographer => photographer.name);
-            const photographerCity = photographers.map(photographer => photographer.city);
-            
-        });
-        let photographers = [
-            {
-                "name": "Ma data test",
-                "id": 1,
-                "city": "Paris",
-                "country": "France",
-                "tagline": "Ceci est ma data test",
-                "price": 400,
-                "portrait": "account.png"
-            },
-            {
-                "name": "Autre data test",
-                "id": 2,
-                "city": "Londres",
-                "country": "UK",
-                "tagline": "Ceci est ma data test 2",
-                "price": 500,
-                "portrait": "account.png"
-            },
-        ]
+        const response = await fetch("./data/photographers.json")
+        const data = await response.json();
+
+        // async function getData() {
+        //     let reponse = await fetch('data/photographers.json');
+        //     let data = (await reponse).json();
+        //     // bien retourner les tableaux une fois récupéré
+        //     return data;
+        // }
+
+        // let photographers = [
+        //     {
+        //         "name": "Ma data test",
+        //         "id": 1,
+        //         "city": "Paris",
+        //         "country": "France",
+        //         "tagline": "Ceci est ma data test",
+        //         "price": 400,
+        //         "portrait": "account.png"
+        //     },
+        //     {
+        //         "name": "Autre data test",
+        //         "id": 2,
+        //         "city": "Londres",
+        //         "country": "UK",
+        //         "tagline": "Ceci est ma data test 2",
+        //         "price": 500,
+        //         "portrait": "account.png"
+        //     },
+        // ]
         // et bien retourner le tableau photographers seulement une fois récupéré
-        return ({
-            photographers: [...photographers, ...photographers, ...photographers]})
+        // return ({
+        //     photographers: [...photographers, ...photographers, ...photographers]})
+        return data;
     }
+
+
+    // const photographers = getPhotographers().then(photographer => photographer);
+    // console.log(photographers);
 
     async function displayData(photographers) {
         const photographersSection = document.querySelector(".photographer_section");
@@ -49,7 +55,7 @@
         // Récupère les datas des photographes
         const { photographers } = await getPhotographers();
         displayData(photographers);
+        console.log(photographers);
     }
-    
     init();
     
