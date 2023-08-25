983cff588a4b07137735b5304cfa05bad5ea8bf5
Gorrog <maximegourgues@protonmail.com>
2023-08-25 11:22:31 +0200

    removed some useless role html attributes & added some more data to the createCard function

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,17 +1,36 @@
 function photographerTemplate(data) {
-    const { name, portrait } = data;
+  const { name, portrait, city, country, tagline, price } = data;
 
-    const picture = `assets/photographers/${portrait}`;
+  const picture = `assets/photographers/photographersId/${portrait}`;
 
-    function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
-        return (article);
-    }
-    return { name, picture, getUserCardDOM }
+  function getUserCardDOM() {
+    const article = document.createElement("article");
+    const img = document.createElement("img");
+    img.classList.add("photographer-portrait");
+    img.setAttribute("src", picture);
+    const h2 = document.createElement("h2");
+    const pCity = document.createElement("p");
+    const pCountry = document.createElement("p");
+    const pTagline = document.createElement("p");
+    const pPrice = document.createElement("p");
+    const locationDiv = document.createElement("div");
+    locationDiv.classList.add("flex");
+    locationDiv.classList.add("gap-1");
+    pPrice.textContent = `${price}€/jour`;
+    pTagline.textContent = tagline;
+    h2.textContent = name;
+    pCity.textContent = `${city},`;
+    pCountry.textContent = country;
+    locationDiv.appendChild(pCity);
+    locationDiv.appendChild(pCountry);
+    article.appendChild(img);
+    article.appendChild(h2);
+    // article.appendChild(pCity);
+    // article.appendChild(pCountry);
+    article.appendChild(locationDiv);
+    article.appendChild(pTagline);
+    article.appendChild(pPrice);
+    return article;
+  }
+  return { name, picture, getUserCardDOM };
 }
